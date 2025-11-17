0b7c29c7c5e1cb3c2e790b49522defa1d86d80c7
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2025-11-17 11:45:22 +0100

    Testowe zaciąganie stron

---
+++ scraper/extractor.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+def extract(page: BeautifulSoup) -> dict:
+    result = {
+        "category": [],
+        "name": "",
+        "price": 0,
+        "description": "",
+        "manufacturer_img": "",
+        "images": []
+        }
+
+    breadcrumb = page.find("div", class_="breadcrumb")
+    if breadcrumb:
+        for crumb in breadcrumb.find_all("a"):
+            result["category"].append(crumb.get_text().strip())
+
+    product_name = page.find("h1", class_="product-title")
+    if product_name:
+        result["name"] = product_name.get_text().strip()
+
+    return result
--- scraper/main.py
+++ scraper/main.py
@@ -1,1 +1,14 @@
-print("biznes")
+from bs4 import BeautifulSoup
+import requests
+
+base_url = "https://sklep.hard-pc.pl"
+site_map = "/sitemap.php"
+pduct_key = "/p"
+
+response = requests.get(base_url + site_map)
+soup = BeautifulSoup(response.content, "html.parser")
+
+product_links = []
+for link in soup.find_all("a", href=True):
+    if base_url + pduct_key in link['href']:
+        product_links.append(link['href'])
