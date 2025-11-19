cbb25267580d099247085f91dc441a95757b3a6e
avvrnk <wronka.agata04@gmail.com>
2025-11-19 14:25:38 +0100

    Fix conflict

--- scraper/extractor.py
+++ scraper/extractor.py
@@ -2,12 +2,9 @@ from bs4 import BeautifulSoup
 
 import elements.name as element_name
 import elements.category as element_category
-<<<<<<< HEAD
 import elements.price as element_price
 import elements.manufacturer_img as element_manufacturer_img
-=======
 import elements.photos as element_photos
->>>>>>> 9229230edf72928d67e6dc55dfd1f9427d83303d
 
 def extract(page: BeautifulSoup) -> dict:
     result = {
@@ -23,11 +20,8 @@ def extract(page: BeautifulSoup) -> dict:
 
     result["category"] = element_category.getCategory(page)
     result["name"] = element_name.getName(page)
-<<<<<<< HEAD
     result["price"] = element_price.getPrice(page)
     result["manufacturer_img"] = element_manufacturer_img.getManufacturerImg(page)
-=======
     result["images"] = element_photos.getPhotos(page)
->>>>>>> 9229230edf72928d67e6dc55dfd1f9427d83303d
 
     return result
